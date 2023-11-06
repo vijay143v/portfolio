ba0c680d315f21e3c491943f6580116383bf77ce
naruto <114555545+vijay143v@users.noreply.github.com>
2023-11-06 19:43:09 +0530

    Add files via upload

---
+++ jh.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing",{
+    strings:["","Heart Hacker","Employee","Time Pass","Enjoyment"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typingg",{
+    strings:["","Heart Hacker","Employee","Time Pass","Enjoyment"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
